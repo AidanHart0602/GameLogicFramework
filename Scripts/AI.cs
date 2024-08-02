b28d2a064bfb52dd2866c1027e4515e63b84ca4c
AidanHart0602 <aidanhart321@gmail.com>
2024-08-01 17:13:34 -1000

    Object Pooling

--- Scripts/AI.cs
+++ Scripts/AI.cs
@@ -7,14 +7,13 @@ public class AI : MonoBehaviour
     [SerializeField]
     private GameObject[] _wayPoints;
     private NavMeshAgent _agent;
-    // Start is called before the first frame update
+
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        transform.position = _wayPoints[0].transform.position;
+        transform.position = _wayPoints[0].transform.position;  
     }
 
-    // Update is called once per frame
     void Update()
     {
         _agent.destination = _wayPoints[1].transform.position;
--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -10,13 +10,35 @@ public class GameManager : MonoBehaviour
         get 
         {
             if(_instance == null)
+            {
                 Debug.LogError("Game manager is NULL");
-            return _instance;
+            }
+                return _instance;    
         }
     }
+
+    [SerializeField]
+    private List<GameObject>_aiPool;
+    [SerializeField]
+    private GameObject _aiPrefab;
+    [SerializeField]
+    private GameObject _aiContainer;
+
     private void Awake()
     {
         _instance = this;
+        _aiPool = StoreAI(10);
     }
 
+    List<GameObject> StoreAI(int NumOfAI)
+    {
+        for (int i = 0; i < NumOfAI; i++)
+        {
+            GameObject AI = Instantiate(_aiPrefab);
+            AI.transform.parent = _aiContainer.transform;
+            AI.SetActive(false);
+            _aiPool.Add(AI);
+        }
+        return _aiPool;
+    }
 }
